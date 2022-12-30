9b8c008dc78b6d4d319c6dc3803972863e649696
KrishanaMohan <ram227209@gmail.com>
2022-12-30 19:29:00 +0530

    ok

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BankAccount b=new BankAccount("krisha",400,200);
-        System.out.println(b.generateAccountNumber(9,9));
+//        BankAccount b=new BankAccount("krisha",400,200);
+//        System.out.println(b.generateAccountNumber(9,9));
     }
 }
