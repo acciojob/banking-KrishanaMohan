be843d9ecebf3ce43f4f971540213a8b338d9ae1
KrishanaMohan <ram227209@gmail.com>
2022-12-30 19:13:43 +0530

    ok

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,8 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        BankAccount b=new BankAccount("krisha",400,200);
+        System.out.println(b.generateAccountNumber(9,9));
     }
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -6,9 +6,16 @@ public class SavingsAccount extends BankAccount{
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
+        super(name,balance,0);
+        this.maxWithdrawalLimit=maxWithdrawalLimit;
+        this.rate=rate;
 
     }
     public void withdraw(double amount) throws Exception {
+        if(amount>maxWithdrawalLimit)
+            throw new UserException("Maximum Withdraw Limit Exceed" );
+        if(amount>getBalance())
+            throw new UserException("Insufficient Balance" );
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
@@ -16,11 +23,13 @@ public class SavingsAccount extends BankAccount{
     }
 
     public double getSimpleInterest(int years){
+        return getBalance()*years*rate;
         // Return the final amount considering that bank gives simple interest on current amount
 
     }
 
     public double getCompoundInterest(int times, int years){
+        return getBalance()*times*years*rate;
         // Return the final amount considering that bank gives compound interest on current amount given times per year
 
     }
